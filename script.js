177106b09d6435afe8fbcc24c2a5b927ec197c03
SergioValero <serva4690@gmail.comgit config --global user.name sergiogit commit -m first>
2025-02-05 10:50:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+
+var numeros = [60,2,3,61,5,6,7]
+
+suma =  0;
+for(let i = 0; i< numeros.length;i++){
+suma = suma + numeros[i];
+
+}
+console.log(suma);
+
+let max = 0;
+
+for(let  i= 0; i< numeros.length;i++){
+    if(numeros[i]>max ){
+    max =numeros[i]
+}
+    }
+    console.log("el mas grande es " + max)
+
+
+
+for(let i = 0; i<numeros.length;i++){
+
+   if(numeros[i] >= 5){
+    numeros[i] + 1;
+    console.log("el numero" +  numeros[i] + "es mayor que 5" + ". el resultado es" + (numeros[i] + 1));
+    }
+   else{
+    numeros[i] - 1;
+    console.log("el numero" + numeros[i] + "es menor que 5" + ".el resultado es" + (numeros[i] - 1));
+   }
+  
+}
+
+console.log(numeros.length);
+
+
+function esPar (numero)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
